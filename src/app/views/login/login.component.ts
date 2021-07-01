1f5fbc13793489eac7976391e6733fb3e428ebd6
Anderson Mori <42013320+AndersonMori@users.noreply.github.com>
2021-06-30 21:41:36 -0300

    Login

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
